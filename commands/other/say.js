ab7571d42c50909505581c902a9e25038e826aa8
chimaerra <danielcatchqq@gmail.com>
2020-02-23 14:08:42 +0700

    Доперевод команды 'say' + некоторые правки (Удаляет отправленное сообщение ;say 'context' если оно было отправлено на сервере) Сделано костыльно. Но ничего не поделать : )

--- commands/other/say.js
+++ commands/other/say.js
@@ -4,21 +4,33 @@ module.exports = class SayCommand extends Command {
   constructor(client) {
     super(client, {
       name: 'say',
-      aliases: ['make-me-say', 'print'],
+      aliases: ['echo', 'print'],
       memberName: 'say',
       group: 'other',
-      description: 'Make the bot say anything',
+      description: 'Заставляет бота сказать что угодно :)',
       args: [
         {
           key: 'text',
-          prompt: 'What do you want the bot to say?',
+          prompt: 'Что мне нужно сказать?',
           type: 'string'
         }
       ]
     });
   }
 
+
   run(message, { text }) {
-    return message.say(text);
+	if(message.channel.type !== 'dm')  
+    message.channel
+      .bulkDelete(1)
+      .then(messages => message.say(text))
+      .catch(e => {
+        console.error(e);
+        return message.say(
+          'Не получилось отправить сообщение :('
+        )
+      })
+	  else if (message.channel.type === 'dm')
+        return message.say(text)
   }
-};
+}
