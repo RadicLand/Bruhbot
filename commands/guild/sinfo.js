df7c1b775376345f1c30e7358922aca64d559ecf
chimaerra <danielcatchqq@gmail.com>
2020-03-06 19:48:10 +0700

    Исправление команды ;sinfo

--- commands/guild/sinfo.js
+++ commands/guild/sinfo.js
@@ -1,23 +1,23 @@
- const { Command } = require('discord.js-commando');
- const { Discord } = require('discord.js');
- const { MessageEmbed } = require('discord.js');
- 
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+
  //author: malokdev - https://github.com/malokdev/Crystal-bot/commit/9485022a306252efe1bb378d928bcba9d0940daa
  
- module.exports = class SinfoCommand extends Command {
+module.exports = class SinfoCommand extends Command {
   constructor(client) {
     super(client, {
       name: 'sinfo',
       aliases: ['serverinfo'],
       memberName: 'sinfo',
       group: 'guild',
-	  guildOnly: true,
-      description: 'Отправляет информацию о текущем сервере'
+	  description: 'Отправляет информацию о текущем сервере',
+	  guildOnly: true
     });
   }
+
  run(message) {
     let region = {
-		"russia": "Россия",
+        "russia": "Россия",
         "brazil": "Бразилия",
         "eu-central": "Центральная Европа",
         "singapore": "Сингапур",
@@ -32,7 +32,7 @@
         "amsterdam": "Амстердам",
         "hongkong": "Гонконг"
     };
-    
+
     var emojis;
     if (message.guild.emojis.size === 0) {
         emojis = 'None';
@@ -41,19 +41,19 @@
     }
 
     const serverinfoEmbed = new MessageEmbed()
-  .setAuthor(message.guild.name, message.guild.iconURL ? message.guild.iconURL : client.user.displayAvatarURL)
-  .setThumbnail(message.guild.iconURL)
-  .setTimestamp()
-  .addField("Создан:", `${message.guild.createdAt.toString().substr(0, 15)}`, true)
-  .addField("ID", message.guild.id, true)
-  .addField("Создатель:", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`, true)
-  .addField("Регион:", region[message.guild.region], true)
-  .addField("Участников:", message.guild.members.filter(m => !m.user.bot).size, true)
-  .addField("Ботов:", message.guild.members.filter(m => m.user.bot).size, true)
-  .addField("Ролей:", message.guild.roles.size, true)
-  .addField("Каналов:", message.guild.channels.size, true)
-  .addField("Эмодзи:", `${emojis}/100`, true)
-  .setColor(Math.floor(Math.random()*16777215))
-  message.channel.send(serverinfoEmbed);
-}
- };
+	   .setThumbnail(message.guild.iconURL())
+	   .setAuthor(message.guild.name, message.guild.iconURL() ? message.guild.iconURL() : this.client.user.displayAvatarURL())
+	   .setTimestamp()
+	   .addField("Создан:", `${message.guild.createdAt.toString().substr(0, 15)}`, true)
+	   .addField("ID", message.guild.id, true)
+	   .addField("Создатель:", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`, true)
+	   .addField("Регион:", region[message.guild.region], true)
+	   .addField("Участников:", message.guild.members.filter(m => !m.user.bot).size, true)
+	   .addField("Ботов:", message.guild.members.filter(m => m.user.bot).size, true)
+	   .addField("Ролей:", message.guild.roles.size, true)
+	   .addField("Каналов:", message.guild.channels.size, true)
+	   .addField("Эмодзи:", `${emojis}/100`, true)
+	   .setColor(Math.floor(Math.random()*16777215))
+	  message.channel.send(serverinfoEmbed);
+  }
+};
